a6639aaf6ffb46518e013ba9f04af19264be3f55
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-09-01 10:54:03 +0700

    FIX: Remove accidentally introduced '23' at the start of the file

--- code/PageWithImage.php
+++ code/PageWithImage.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
 * Defines the SupportingProjectPage page type - initial code created by ss generator
 */
@@ -32,11 +32,12 @@ class PageWithImage extends Page implements RenderableAsPortlet {
 
   // FIXME - make this more efficient
   public function getPortletImage() {
-    $result = '';
-    if ($this->MainImageID) {
+    $result = null;
+    if ($this->MainImageId) {
       $result = DataObject::get_by_id('Image', $this->MainImageID);
     }
-    return $result; 
+
+    return $result;
   }
   
   
