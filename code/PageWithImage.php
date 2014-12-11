a7aaeaf70f864c24cb371ba87b918828d43d5d37
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-12-11 16:17:05 +0700

    ENHANCEMENT: Ensure page with image is twitter cardable

--- code/PageWithImage.php
+++ code/PageWithImage.php
@@ -2,7 +2,10 @@
 /**
 * Defines the SupportingProjectPage page type - initial code created by ss generator
 */
-class PageWithImage extends Page implements RenderableAsPortlet {
+class PageWithImage extends Page implements RenderableAsPortlet, RenderableAsTwitterCard {
+
+  // Brief summary to show on facebook and twitter when linking
+  private static $db = array('BriefIntroduction' => 'Text');
 
 
   static $has_one = array(
@@ -22,6 +25,7 @@ class PageWithImage extends Page implements RenderableAsPortlet {
     $fields->addFieldToTab( 'Root.'.$imagename, $uf = new UploadField('MainImage', _t('PageWithImage.MAIN_IMAGE', 'Main Image')));
     $dirname = strtolower($this->ClassName).'s';
     $uf->setFolderName($dirname);
+    $fields->addFieldToTab('Root', new TextAreaField('BriefIntroduction', 'Brief description of the page for Twitter and Facebook purposes'), 'Content');
     return $fields;
   }
 
@@ -33,17 +37,56 @@ class PageWithImage extends Page implements RenderableAsPortlet {
 
   // FIXME - make this more efficient
   public function getPortletImage() {
-    $result = null;
-    if ($this->MainImageID) {
-      $result = DataObject::get_by_id('Image', $this->MainImageID);
-    }
-    return $result;
+    return $this->MainImage();
   }
   
   
  
   public function getPortletCaption() {
-    return '';
+    return $this->BriefIntroduction;
+  }
+
+
+
+    /**
+   * An accessor method for the title of an item in a Twitter
+   * @example
+   * <code>
+   *  return $this->Title;
+   * </code>
+   *
+   * @return string
+   */
+  public function getTwitterTitle() {
+    return $this->Title;
+  }
+  
+
+  /**
+   * An accessor method for an image for a Twitter
+   * @example
+   * <code>
+   *  return $this->NewsItemImage;
+   * </code>
+   *
+   * @return string
+   */
+  public function getTwitterImage() {
+    return $this->MainImage();
+  }
+  
+  
+  /**
+   * An accessor for text associated with the Twitter
+   * @example
+   * <code>
+   * return $this->Summary
+   * </code>
+   *
+   * @return string
+   */ 
+  public function getTwitterDescription() {
+    return $this->BriefIntroduction;
   }
 
 }
