73f54cf458aa12d55d38dda05ad82727a92f28c1
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-05-28 13:58:06 +0700

    FIX: No need to have PageWithImageFolder as an allowed child as anything which extends PageWithImage works

--- code/PageWithImageFolder.php
+++ code/PageWithImageFolder.php
@@ -2,9 +2,27 @@
 /**
 * Defines the SupportingProjectPage page type - initial code created by ss generator
 */
-class PageWithImageFolder extends PageWithImage {
+class PageWithImageFolder extends PageWithImage implements SeoInformationProvider {
 
-	static $allowed_children = array('PageWithImage', 'PageWithImageFolder');
+	static $allowed_children = array('PageWithImage');
+
+
+	public function getImagesForSeo() {
+		$result = new ArrayList();
+		$pwis = $this->owner->AllChildren()->where("ClassName in (
+			'PageWithImage','PageWithImagesFolder')");
+		foreach ($pwis->getIterator() as $pwi) {
+			if ($pwi->MainImageID > 0) {
+				$result->push($pwi->MainImage());
+			}
+		}
+		return $result;
+	}
+
+
+	public function getLinksForSeo() {
+		return false;
+	}
 
 }
 
